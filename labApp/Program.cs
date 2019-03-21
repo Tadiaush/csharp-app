5f2814c379ad8807a32a2e7c47b38a217e94c65f
Tadas <tadas@Tadass-MacBook-Pro.local>
2019-03-21 23:14:53 +0200

    Trying to finish the v0.1

--- labApp/Program.cs
+++ labApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace labApp
 {
@@ -6,17 +7,35 @@ namespace labApp
     {
         static void Main(string[] args)
         {
+            string s_vardas, s_pav;
+            int ndSkaicius;
+            int s_namuDarbas=0, s_egzRezul, ndPaz;
+            double s_galPaz, s_ndMedian;
+
+
             Console.WriteLine("Iveskite studento varda ir pavarde");
-            string s_vardas = Console.ReadLine();
-            string s_pav = Console.ReadLine();
+            s_vardas = Console.ReadLine();
+            s_pav = Console.ReadLine();
             
-            Console.WriteLine("Iveskite studento namu darbu ir egzamino rezultata:");
-            double s_namuDarbas = Convert.ToDouble(Console.ReadLine());
-            double s_egzRezul = Convert.ToDouble(Console.ReadLine());
-            //isivedame galutinio pazymo kintamaji ir skaiciuojame pagal pateikta formule. 
-            double s_galPaz; 
-            s_galPaz = (0.3 * s_namuDarbas) + (0.7 * s_egzRezul);
-          
+            List<int> ndPazymiai = new List<int>();
+            Console.WriteLine("Namu darbai:" + "\n" + "Namu darbu skaicius" + "\n");
+            ndSkaicius = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Namu darbu pazymiai:");
+            for (int i=0;i<ndSkaicius;i++){
+                ndPaz = Convert.ToInt16(Console.ReadLine());
+                ndPazymiai.Add(ndPaz);
+                s_namuDarbas += ndPaz;
+            };
+
+            Console.WriteLine("Iveskite studento egzamino rezultata:");
+            s_egzRezul = Convert.ToInt16(Console.ReadLine());
+
+            //Calculate the main grade.
+            s_galPaz = (0.3 * (s_namuDarbas / ndPazymiai.Count)) + (0.7 * s_egzRezul);
+            //s_galPaz = (0.3 * s_namuDarbas) + (0.7 * s_egzRezul);
+            s_ndMedian = GetMedian(Convert.ToDouble(ndPazymiai));
+
+            
             //Formating the line, so everything could in a columns. 
             Console.WriteLine("Vardas".PadRight(15,' ') + 
                               "Pavarde".PadRight(15,' ') +
@@ -31,5 +50,19 @@ namespace labApp
                               s_pav.PadRight(15,' ') +
                               s_galPaz.ToString().PadRight(10,' '));
         }
+        public static double GetMedian (List<int> sarasas) {
+
+        int numberCount = sarasas.Count;
+        int halfCount = numberCount/2;
+        sarasas.Sort();
+        
+        double median;
+
+        if ((numberCount % 2) == 0) {
+            median = ((sarasas.Find(numberCount)))
+        }
+        
+        
+    }
     }
 }
