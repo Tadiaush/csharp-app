6ea8a3f0148deccb94d5b85b4a4944879d9597c4
Tadas <tadas@Tadass-MacBook-Pro.local>
2019-03-21 20:33:13 +0200

    Created C# app via DotNet SDK

---
+++ labApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace labApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
