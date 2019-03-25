107743f02a56dd2fefd0b61ebf56d1f5a087288e
Tadas <tadas@Tadass-MacBook-Pro.local>
2019-03-25 21:23:36 +0100

    Fixed the result and added Median calculation

--- labApp/Program.cs
+++ labApp/Program.cs
@@ -9,19 +9,22 @@ namespace labApp
         {
             string s_vardas, s_pav;
             int ndSkaicius;
-            int s_namuDarbas=0, s_egzRezul, ndPaz;
+            int s_namuDarbas = 0, s_egzRezul, ndPaz;
             double s_galPaz, s_ndMedian;
-
+            List<int> ndPazymiai = new List<int>();
 
             Console.WriteLine("Iveskite studento varda ir pavarde");
             s_vardas = Console.ReadLine();
             s_pav = Console.ReadLine();
-            
-            List<int> ndPazymiai = new List<int>();
+
+
             Console.WriteLine("Namu darbai:" + "\n" + "Namu darbu skaicius" + "\n");
             ndSkaicius = Convert.ToInt16(Console.ReadLine());
+            Console.ReadLine();
+
             Console.WriteLine("Namu darbu pazymiai:");
-            for (int i=0;i<ndSkaicius;i++){
+            for (int i = 0; i < ndSkaicius; i++)
+            {
                 ndPaz = Convert.ToInt16(Console.ReadLine());
                 ndPazymiai.Add(ndPaz);
                 s_namuDarbas += ndPaz;
@@ -32,37 +35,36 @@ namespace labApp
 
             //Calculate the main grade.
             s_galPaz = (0.3 * (s_namuDarbas / ndPazymiai.Count)) + (0.7 * s_egzRezul);
-            //s_galPaz = (0.3 * s_namuDarbas) + (0.7 * s_egzRezul);
-            s_ndMedian = GetMedian(Convert.ToDouble(ndPazymiai));
 
-            
+            int[] pazMasyv = ndPazymiai.ToArray();
+            s_ndMedian = GetMedian(pazMasyv);
+
             //Formating the line, so everything could in a columns. 
-            Console.WriteLine("Vardas".PadRight(15,' ') + 
-                              "Pavarde".PadRight(15,' ') +
-                              "Galutinis(Vid.)".PadRight(10,' '));
+            Console.WriteLine("Vardas".PadRight(15, ' ') +
+                              "Pavarde".PadRight(15, ' ') +
+                              "Galutinis(Vid.)".PadRight(10, ' ') +
+                              "/".PadRight(2, ' ') +
+                              "Galutinis (Med.)".PadRight(10, ' '));
             //Creating one line full of dash
-            for (int i=0;i<45;i++){
+            for (int i = 0; i < 61; i++)
+            {
                 Console.Write("-");
             }
             Console.WriteLine();
             //Padding the values to the right with enough of space. 
-            Console.WriteLine(s_vardas.PadRight(15,' ') +
-                              s_pav.PadRight(15,' ') +
-                              s_galPaz.ToString().PadRight(10,' '));
+            Console.WriteLine(s_vardas.PadRight(15, ' ') +
+                              s_pav.PadRight(15, ' ') +
+                              s_galPaz.ToString("0.00").PadRight(10, ' ') +
+                              s_ndMedian.ToString("0.00").PadRight(12, ' '));
         }
-        public static double GetMedian (List<int> sarasas) {
+        public static double GetMedian(int[] sarasas)
+        {
 
-        int numberCount = sarasas.Count;
-        int halfCount = numberCount/2;
-        sarasas.Sort();
-        
-        double median;
+            int size = sarasas.Length;
+            int mid = size / 2;
+            double median = (size % 2 != 0) ? (double)sarasas[mid] : ((double)sarasas[mid] + (double)sarasas[mid - 1]) / 2;
+            return median;
 
-        if ((numberCount % 2) == 0) {
-            median = ((sarasas.Find(numberCount)))
         }
-        
-        
-    }
     }
 }
