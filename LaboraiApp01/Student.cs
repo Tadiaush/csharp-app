3dd1c30b757291fab406122e6fcfc1e34564eb9f
Tadiaush <tadas.bobinas@gmail.com>
2019-04-19 20:01:47 +0300

    Pushing the refractor code from main PC

---
+++ LaboraiApp01/Student.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboraiApp01
+{
+    public class Student
+    {
+        int id { get; set; }
+        string studFName { get; set; }
+        string studLName { get; set; }
+        List<int> studND = new List<int>();
+        int studEgz { get; set; }
+
+        public Student(int id, string studFName, string studLName, List<int> studND, int studEgz)
+        {
+            this.id = id;
+            this.studFName = studFName;
+            this.studLName = studLName;
+            this.studND = studND;
+            this.studEgz = studEgz;
+
+        }
+        public Student()
+        {
+            this.id = id;
+            this.studFName = studFName;
+            this.studLName = studLName;
+            this.studND = studND;
+            this.studEgz = studEgz;
+        }
+
+        public double GradesCalculationAvg(int exam, int[] grade)
+        {
+            string choice = "avg";
+            Calculation calc = new Calculation();
+            double calcgradeavg = calc.GetFinal(choice, exam, grade);
+            return calcgradeavg;
+        }
+        public double GradesCalculationMed(int exam, int[] grade)
+        {
+            string choice = "med";
+            Calculation calc = new Calculation();
+            double calcgrademed = calc.GetFinal(choice, exam, grade);
+            return calcgrademed;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine("{0,0:C}{1,14:C}{2,28:N}{3,17:N}",
+            //Console.WriteLine("{0,0:C}{1,15:C}{2,15:N}{3,15:N}",
+              this.studFName,
+              this.studLName,
+              GradesCalculationAvg(this.studEgz, this.studND.ToArray()),
+              GradesCalculationMed(this.studEgz, this.studND.ToArray())
+            );
+        }
+
+
+
+
+        // public double medianGet()
+        // {
+        //     return this.median;
+        // }
+        // public double galPazymysGet()
+        // {
+        //     return this.galPazymys;
+        // }
+
+
+        // public String PrintOut()
+        // {
+        //     this.median = GetMedian(this.studND.ToArray());
+        //     this.galPazymys = (0.3 * StuPaz(this.studND.ToArray())) + (0.7 * this.studEgz);
+
+        //     /* return (studFName.PadRight(15, ' ') +
+        //             studLName.PadRight(15, ' ') +
+        //             galPazymys.ToString("0.00").PadRight(15, ' ') +
+        //             median.ToString("0.00").PadRight(10, ' ')
+        //             );*/
+
+        //     //return (String.Format("(0,-15) (1,0) (2, 10) (3,20)", studFName, studLName, galPazymys, median));
+        //     return ($"{studFName} {studLName} {galPazymys} {median}");
+        // }
+        //The input is array of homework grades. 
+        // public double StuPaz(int[] pazymiai)
+        // {
+
+        //     try
+        //     {
+        //         int vidurkis = 0, sumPazymiai = 0;
+        //         for (int i = 0; i < pazymiai.Length; i++)
+        //         {
+        //             //studND.Add(pazymiai[i]);
+
+        //             sumPazymiai += pazymiai[i];
+        //         }
+        //         vidurkis = sumPazymiai / pazymiai.Length;
+        //         return vidurkis;
+
+        //     }
+        //     catch (DivideByZeroException ex)
+        //     {
+        //         //LogError(ex):
+        //         Console.WriteLine("Negalima atlikti dalybos veiksmo is nulio");
+        //         return 0;
+        //     }
+
+
+        // }
+        //The inout is array of homework grades. 
+        // public static double GetMedian(int[] sarasas)
+        // {
+
+        //     int size = sarasas.Length;
+        //     int mid = size / 2;
+        //     double median = (size % 2 != 0) ? (double)sarasas[mid] : ((double)sarasas[mid] + (double)sarasas[mid - 1]) / 2;
+        //     return median;
+
+        // }
+    }
+}
---
+++ LaboraiApp01/source.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboraiApp01
+{
+    class source
+    {
+        static void Main(string[] args)
+        {
+            //Global kintamieji:
+            Random rnd = new Random();
+            char caseSwitch;
+
+            List<Student> stud = new List<Student>();
+            List<int> allhomeworkgrades = new List<int>();
+            string studfname, studlname, grade;
+            int count, countgrades, examresult;
+
+            PrintingOutput printout = new PrintingOutput();
+
+            //Choise to enter the input data
+            Console.WriteLine("Input choice" + "\n" + "C - through Terminal" + " ; F - input file");
+            caseSwitch = Convert.ToChar(Console.ReadLine().ToLower());
+            //caseSwitch = 'f';
+
+            switch (caseSwitch)
+            {
+                case 'c':
+                    Console.WriteLine("How many students? ");
+                    count = Convert.ToInt16(Console.ReadLine());
+
+                    for (int i = 0; i < count; i++)
+                    {
+                        Console.WriteLine("Input data must be in this format:"
+                                        + "\n"
+                                        + "First name Last name");
+                        string row = Console.ReadLine();
+                        string[] word = row.Split(new char[0]);
+                        studfname = word[0];
+                        studlname = word[1];
+
+                        Console.WriteLine("How many grades does the student has?");
+                        countgrades = Convert.ToInt16(Console.ReadLine());
+
+                        for (int j = 0; j < countgrades; j++)
+                        {
+                            Console.WriteLine("Insert the grade: ");
+                            grade = Console.ReadLine();
+
+                            if (grade != " ")
+                            {
+                                allhomeworkgrades.Add(Convert.ToInt16(grade));
+                            }
+                            else
+                            {
+                                allhomeworkgrades.Add(rnd.Next(1, 10));
+                            }
+                        }
+                        Console.WriteLine("Insert student final exam grade:");
+                        examresult = Convert.ToInt16(Console.ReadLine());
+
+                        stud.Add(new Student(i, studfname, studlname, allhomeworkgrades, examresult));
+                    }
+             
+                    printout.ResultLines();
+                    printout.StudentGrades(stud);
+
+                    Console.ReadLine();
+                    break;
+
+                case 'f':
+                    break;
+
+
+            }
+        }
+    }
+}
